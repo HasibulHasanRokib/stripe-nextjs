20818595224292d6deb64585c1c41a8e627f009d
Hasibul Hasan Rokib <rokib4000@gmail.com>
2026-04-17 01:06:38 +0600

    fix: remove async from Page component declaration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default async function Page() {
+export default function Page() {
   return (
     <div className="min-h-screen flex flex-col gap-8 justify-center items-center">
       <h1 className="font-bold text-6xl leading-relaxed">
